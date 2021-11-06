42bb958f9cacca1b5131e45a5aeb9d401707fdca
Don_Ramsey <don_ramsey@github.com>
2021-11-06 09:03:45 -0400

    Setup app for Heroku

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send("Welcome to the Home Page!")
+  res.send("Welcome to the My Heroku Home Page!")
 });
 
-app.listen(3000, () => {
-  // console.log("Wazzzzapppp");
+app.listen(port, () => {
+  console.log('Server is up on port ' + port)
 });
 
